f80169905d80515329b5d35db8caa18b05fcb935
Scott <spfrood@gmail.com>
2018-11-16 14:19:56 -0800

    updated_python
    Updated python from 3.6 to 3.7, added 2.x and 3.x sandbox directories,

--- sandbox-py3x/python_proj_hello/src/python_proj_hello.py
+++ sandbox-py3x/python_proj_hello/src/python_proj_hello.py
@@ -2,8 +2,8 @@
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
 
-__author__ = "scott_r_parker"
-__date__ = "$Jul 10, 2017 4:41:45 PM$"
+print ("Hello World")
+print ("Not in my backyard!")
 
-if __name__ == "__main__":
-    print "Hello World"
+print (7 / 5)
+print (-7 / 5)
