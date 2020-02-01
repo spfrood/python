0b7fe5ac0d82d944cca9f5c0ac6f323080692795
spfrood <spfrood@gmail.com>
2020-02-01 15:27:44 -0800

    Create logging.py

---
+++ iepug-meetup/logging.py
@@ -0,0 +1,11 @@
+
+# Socratica video for reference:
+# Search on youtube for python logging modules
+# in python sandbox enter following commands:
+# >>> import logging
+# >>> dir(logging)
+
+import logging
+
+
+
